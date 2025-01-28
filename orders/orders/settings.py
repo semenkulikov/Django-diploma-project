91bf58ba921a9389799914b4f839680e156ad232
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-28 21:59:15 +0300

    Work completed

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -97,22 +97,22 @@ WSGI_APPLICATION = "orders.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv("ENGINE"),
-#         "NAME": os.getenv("NAME"),
-#         "HOST": os.getenv("HOST"),
-#         "PORT": os.getenv("PORT"),
-#         "USER": os.getenv("USER"),
-#         "PASSWORD": os.getenv("PASSWORD"),
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'diploma',
+    "default": {
+        "ENGINE": os.getenv("ENGINE"),
+        "NAME": os.getenv("NAME"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'diploma',
+#     }
+# }
 
 AUTH_USER_MODEL = "backend.User"
 
